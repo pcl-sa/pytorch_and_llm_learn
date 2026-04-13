0d7036694d62fdb4d4f7f41e3d6992a7f5bdde17
LPC <15348475109@163.com>
2026-04-14 00:00:08 +0800

    完善transformer中的__init__.py文件，简化调用

--- transformer_project/model/__init__.py
+++ transformer_project/model/__init__.py
@@ -2,3 +2,7 @@
 # 包含了transformer模型的所有组件，如编码器、解码器、注意力机制等
 # 基于pytorch框架实现
 
+#只向外提供transformer模型,以及Encoder和Decoder组件
+from .encoder import EncoderLayer
+from .decoder import DecoderLayer
+from .transformer import Transformer, Encoder, Decoder
